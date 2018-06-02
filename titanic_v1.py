8e2ffb1722bdcf8949c01311a44dca3c91d7e47c
ayaanle-hashi <ayaanlehashii@gmail.com>
2018-06-02 21:16:41 +0100

    second initial commit

---
+++ titanic_v1.py
@@ -0,0 +1,56 @@
+import pandas as pd
+from sklearn import preprocessing
+from sklearn.neighbors import KNeighborsClassifier
+
+data = pd.read_csv("train.csv", index_col=0)
+test_data = pd.read_csv("test.csv",index_col=0)
+
+print("Cleaning Data...")
+
+clean_data = data.drop(['Name','Ticket','Cabin'], axis=1)
+final_data = clean_data.copy()
+final_data['Age'] = final_data['Age'].fillna(final_data['Age'].median())
+final_data = final_data.dropna()
+
+clean_test_data = test_data.drop(['Name','Ticket','Cabin'], axis=1)
+final_test_data = clean_test_data.copy()
+final_test_data['Age'] = final_test_data['Age'].fillna(final_test_data['Age'].median())
+final_test_data['Fare'] = final_test_data['Fare'].fillna(final_test_data['Fare'].median())
+final_test_data = final_test_data.dropna()
+
+male = 0
+female = 1
+labels = ['male','female']
+encoder = preprocessing.LabelEncoder()
+encoder.fit(labels)
+final_data['Sex'] = encoder.fit_transform(final_data['Sex'])
+final_test_data['Sex'] = encoder.fit_transform(final_test_data['Sex'])
+
+S = 0
+C = 1
+Q = 2
+label = ['S','C','Q']
+Embarkedencoder = preprocessing.LabelEncoder()
+Embarkedencoder.fit(label)
+final_data['Embarked'] = Embarkedencoder.fit_transform(final_data['Embarked'].astype(str))
+final_test_data['Embarked'] = Embarkedencoder.fit_transform(final_test_data['Embarked'].astype(str))
+
+test_label = final_data.loc[:,'Survived']
+X = final_data.loc[:, final_data.columns != 'Survived']
+y = final_data.loc[:, 'Survived']
+knn = KNeighborsClassifier(n_neighbors=10)
+knn.fit(X,y)
+
+print(knn.fit(X,y))
+
+titanic_prediction = final_test_data.copy()
+print(titanic_prediction.dtypes)
+knn.predict(titanic_prediction)
+titanic_prediction['Survived'] = knn.predict(titanic_prediction)
+print("Predicting some stuff...")
+print("Here is the output")
+print(knn.predict(final_test_data))
+print(titanic_prediction)
+test_prediction = titanic_prediction ['Survived']
+print(titanic_prediction.dtypes)
+titanic_prediction.to_csv('hello.csv', header=True)
---
+++ titanic_v2.py
@@ -0,0 +1,88 @@
+import pandas as pd
+from sklearn import preprocessing
+from sklearn.neighbors import KNeighborsClassifier
+
+data = pd.read_csv("train.csv", index_col=0)
+test_data = pd.read_csv("test.csv",index_col=0)
+
+print("Cleaning Data...")
+
+clean_data = data.drop(['Name','Ticket'], axis=1)
+final_data = clean_data.copy()
+final_data['Age'] = final_data['Age'].fillna(final_data['Age'].median())
+final_data['Deck'] = final_data['Cabin'].str.slice(0,1)
+final_data['Room'] = final_data['Cabin'].str.slice(1,5).str.extract('([0-9]+)',expand=False).astype('float')
+final_data = final_data.drop(['Cabin'], axis = 1)
+final_data ['Deck'] = final_data['Deck'].fillna('U')
+final_data['Room'] = final_data['Room'].fillna(final_data['Room'].median())
+
+clean_test_data = test_data.drop(['Name','Ticket'], axis=1)
+final_test_data = clean_test_data.copy()
+final_test_data['Age'] = final_test_data['Age'].fillna(final_test_data['Age'].median())
+final_test_data['Deck'] = final_test_data['Cabin'].str.slice(0,1)
+final_test_data['Room'] = final_test_data['Cabin'].str.slice(1,5).str.extract('([0-9]+)',expand=False).astype('float')
+final_test_data = final_test_data.drop(['Cabin'], axis = 1)
+final_test_data ['Deck'] = final_test_data['Deck'].fillna('U')
+final_test_data['Fare'] = final_test_data['Fare'].fillna(7.225)
+final_test_data['Room'] = final_test_data['Room'].fillna(final_test_data['Room'].median())
+
+A = 0
+B = 1
+C = 2
+D = 3
+E = 4
+F = 5
+G = 6
+T = 7
+U = 8
+
+labels_cabin = ['U','C','B','D','E','A','F','G','T']
+Cabinencoder = preprocessing.LabelEncoder()
+Cabinencoder.fit(labels_cabin)
+final_data['Deck'] = Cabinencoder.fit_transform(final_data['Deck'])
+
+A = 0
+B = 1
+C = 2
+D = 3
+E = 4
+F = 5
+G = 6
+U = 7
+
+labels_cabin = ['U','C','B','D','E','A','F','G']
+Cabinencoder = preprocessing.LabelEncoder()
+Cabinencoder.fit(labels_cabin)
+final_test_data['Deck'] = Cabinencoder.fit_transform(final_test_data['Deck'])
+
+female = 0
+male = 1
+labels = ['male','female']
+encoder = preprocessing.LabelEncoder()
+encoder.fit(labels)
+final_data['Sex'] = encoder.fit_transform(final_data['Sex'])
+final_test_data['Sex'] = encoder.fit_transform(final_test_data['Sex'])
+
+C = 0
+Q = 1
+S = 2
+label = ['S','C','Q']
+Embarkedencoder = preprocessing.LabelEncoder()
+Embarkedencoder.fit(label)
+final_data['Embarked'] = Embarkedencoder.fit_transform(final_data['Embarked'].astype(str))
+final_test_data['Embarked'] = Embarkedencoder.fit_transform(final_test_data['Embarked'].astype(str))
+
+X = final_data.loc[:, final_data.columns != 'Survived']
+y = final_data.loc[:, 'Survived']
+knn = KNeighborsClassifier(n_neighbors=10)
+knn.fit(X,y)
+
+titanic_prediction = final_test_data.copy()
+print("Predicting some stuff...")
+titanic_prediction['Survived'] = knn.predict(titanic_prediction)
+print("Here is the output")
+print(knn.predict(final_test_data))
+print(titanic_prediction)
+print(titanic_prediction.dtypes)
+titanic_prediction = titanic_prediction.drop(['Pclass','Sex','Age','SibSp','Parch','Fare','Embarked','Deck','Room'], axis=1)
+titanic_prediction.to_csv('hello.csv', header=True)
